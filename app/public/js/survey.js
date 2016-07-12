b53f6e6462088f6ba84540a10710c8f293387ce6
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-07-12 01:10:57 -0400

    more survey navigation

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
+	var qCount = 0;
+
 	$('.nextQ').click(function() {
-			nextQ();
+		nextQ();
+		qCount += 1;
 	});
 
 	function nextQ(){ 
@@ -11,12 +14,13 @@ $(document).ready(function(){
 			var posConvert = (pos.left / windowSize) * 100;
 			var newPos = posConvert - 100;
 			$("#box" + i).animate({left: newPos + "%"}, 750);
-			//console.log("Q" + i + ": " + posConvert);
+			console.log("Q" + i + ": " + posConvert);
 		}
 	}
 
 	$('.backQ').click(function() {
 		backQ();
+		qCount -= 1;
 	});
 
 	function backQ(){ 
@@ -26,9 +30,45 @@ $(document).ready(function(){
 			var posConvert = (pos.left / windowSize) * 100;
 			var newPos = posConvert + 100;
 			$("#box" + i).animate({left: newPos + "%"}, 750);
-			//console.log("Q" + i + ": " + posConvert);
+			console.log("Q" + i + ": " + posConvert);
 		}
 	}
+
+	$('.firstQ').click(function() {
+		firstQ();
+	});
+
+	function firstQ(){ 
+		for (i = 1; i <= 10; i++){
+			var windowSize = $(window).width();
+			var pos = $("#box" + i).position();
+			var posConvert = (pos.left / windowSize) * 100;
+			var currQ = qCount * 100
+			var newPos = posConvert + currQ;
+			$("#box" + i).animate({left: newPos + "%"}, 1750);
+			console.log("Q" + i + ": " + newPos);
+		}
+		qCount = 0;
+	}
+
+	$('.lastQ').click(function() {
+		lastQ();
+	});
+
+	function lastQ(){
+		var qToFinish = 9 - qCount; 
+		for (i = 1; i <= 10; i++){
+			var windowSize = $(window).width();
+			var pos = $("#box" + i).position();
+			var posConvert = (pos.left / windowSize) * 100;
+			var currQ = qToFinish * 100
+			var newPos = posConvert - currQ;
+			$("#box" + i).animate({left: newPos + "%"}, 1750);
+			console.log("Q" + i + ": " + newPos);
+		}
+		qCount = 9;
+	}
+
 
 });
 
