f348bdda0b6df286d0b82cd57aa11047e720f758
Matt Owesn <mattwowens@ymail.com>
2016-07-14 21:07:16 -0400

    Commenting explanations for logic in javascript files

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -11,13 +11,11 @@ module.exports = function(app){
 		res.json(beerData)
 	});
 
+
 	app.post('/api/data', function(req, res){
 
 		var userData = req.body;
 		var userLocation = userData.city;
-
-/*		beerData.push(userData);*/
-
 
 		//Geocodes user input (to long/lat)
 		geocoder.geocode(userLocation, function ( err, data ) {
@@ -28,8 +26,10 @@ module.exports = function(app){
 
 		});
 
+		//Takes geocoded location to use in BreweryDB API search for nearby breweries.  Searches in 35 mile radius.
 		function findBreweries(lat, long) {
 
+			//Building URL for BreweryDB API.
 			var url = "https://api.brewerydb.com/v2/search/geo/point?";
 			var key = "&key=ab12f84ebc125fbbfd15ff211dd304e6&format=json"
 			url += "lat=" + lat;
@@ -38,32 +38,25 @@ module.exports = function(app){
 			url += key;
 
 			request(url, function (error, response, body) {
-			  if (!error && response.statusCode == 200) {
-			  	var breweryData = JSON.parse(body)
-			  	var numOfResults = breweryData.totalResults;
+				if (!error && response.statusCode == 200) {
+				  	var breweryData = JSON.parse(body)
+				  	var numOfResults = breweryData.totalResults;
 
+				  	//Dynamically creating brewery objects containing only select information from breweries, in hopes to reduce payload of our API page.
+				  	for (var i = 0; i < numOfResults; i++) {
+				  		var selectedBreweryData = {
+				  			breweryName: breweryData.data[i].brewery.name,
+				  			latitude: breweryData.data[i].latitude,
+				  			longitude: breweryData.data[i].longitude,
+				  			description: breweryData.data[i].brewery.description,
+				  			typeOfBrewery: breweryData.data[i].locationTypeDisplay
 
-			  	for (var i = 0; i < numOfResults; i++) {
-			  		var selectedBreweryData = {
-			  			breweryName: breweryData.data[i].brewery.name,
-			  			latitude: breweryData.data[i].latitude,
-			  			longitude: breweryData.data[i].longitude,
-			  			description: breweryData.data[i].brewery.description,
-			  			typeOfBrewery: breweryData.data[i].locationTypeDisplay
-
-			  		}
-			  		beerData.push(selectedBreweryData)
-			  	}
-
-			  }
+				  		}
+				  		//Pushing above object to our API for use on front-end.
+				  		beerData.push(selectedBreweryData)
+				  	}
+				}
 			})
 		}
-		// https://api.brewerydb.com/v2/search/geo/point?lat=35.772096&lng=-78.638614&key=ab12f84ebc125fbbfd15ff211dd304e6&format=json
-
-
-
-
 	})
-
-
 }
