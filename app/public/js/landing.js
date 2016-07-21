519bd12f89e3628623d7ab370cb99fc7e89dcaa1
John Tracy <johnmtracy133@hotmail.com>
2016-07-21 19:19:07 -0400

    commented out forgot password function

--- app/public/js/landing.js
+++ app/public/js/landing.js
@@ -111,14 +111,6 @@ $('#submit-forgot').on('click', function(){
 				var password = response.userPass;
 				var email = response.userEmail;
 				$('#submit-forgot').remove();
-				$.ajax({
-					method: 'POST',
-					url: currentUrl + '/test',
-					data: {
-						password: password,
-						email: email 
-					}
-				})
 			}
 			else if(response == 'invalid'){
 				console.log('this is not a valid username');
